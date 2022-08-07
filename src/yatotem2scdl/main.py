c63ca36fd42add2285b27aee2268658918d70eed
gautierr <romain.gautier@sib.fr>
2022-08-07 22:52:50 +0200

    Better CLI

--- src/yatotem2scdl/main.py
+++ src/yatotem2scdl/main.py
@@ -22,6 +22,7 @@ def main():
     pdc_envname = "PLANS_DE_COMPTES_DIR"
 
     parser = argparse.ArgumentParser(description="Convertit un fichier totem en SCDL")
+    parser.add_argument("nature_acte", type=str, help="Nature de l'acte (seul la valeur budget est supporté pour le moment)")
     parser.add_argument("totem_file", type=str, help="Chemin du fichier totem")
     parser.add_argument(
         pdc_argname,
@@ -35,12 +36,17 @@ def main():
 
     status = 0
 
+    if args.nature_acte != "budget":
+        sys.stderr.write("Seul les budgets sont supportés.")
+        sys.exit(-1)
+    
     if args.plans_de_comptes is None:
         sys.stderr.write("Vous devez spécifier oú se trouvent les plans de comptes ")
         sys.stderr.write(
             f"via l'argument {pdc_argname} ou la variable d'environnement {pdc_envname}"
         )
         sys.stderr.write("\n")
+        sys.exit(-1)
 
     try:
         process(args)
